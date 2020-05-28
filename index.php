83135256ccb321a806eec43e208c0369ce7fa113
dimon-camelot <65960488+dimon-camelot@users.noreply.github.com>
2020-05-28 13:04:23 +0300

    изменен sql запрос на выгрузку 4 последних постов

--- index.php
+++ index.php
@@ -20,9 +20,9 @@ $password = 'root'; // пароль
 $link = mysqli_connect($host, $user, $password, $database)
 or die("Ошибка " . mysqli_error($link));
 
-// достаем посты из базы, начиная с самого свежего
+// достаем 4 поста из базы, начиная с самого свежего
 
-$sql = "SELECT id, title, content, date_time, category_id FROM posts ORDER BY date_time DESC";
+$sql = "SELECT id, title, content, date_time, category_id FROM posts ORDER BY date_time DESC LIMIT 3";
 
 $posts = makeSelectFromDB($link, $sql);
 
@@ -70,13 +70,10 @@ mysqli_close($link);
                 $i = 0;
                 foreach ($posts as $value) {
 
-                    if ($i < 4) { //Количество постов, отображаемых на странице
-                        $i++;
-                        echo "<h5>{$value['title']}</h5>";
-                        echo "<p>{$value['date_time']}</p>";
-                        echo "<p>{$value['content']}</p>";
-                        echo '<br>';
-                    }
+                    echo "<h5>{$value['title']}</h5>";
+                    echo "<p>{$value['date_time']}</p>";
+                    echo "<p>{$value['content']}</p>";
+                    echo '<br>';
                 }
                 ?>
 
